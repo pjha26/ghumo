1e73812f6a0b9005904a6926e7f56cb0c4db213f
pjha26 <praj80624@gmail.com>
2025-11-24 19:22:34 +0530

    added map feature

--- src/components/Listing/MapWrapper.js
+++ src/components/Listing/MapWrapper.js
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const GoogleMap = dynamic(() => import('./GoogleMap'), {
+const GoogleMap = dynamic(() => import('./Map'), {
     ssr: false,
     loading: () => <div style={{ height: '400px', width: '100%', background: '#f0f0f0', borderRadius: '12px', marginTop: '24px' }}>Loading Map...</div>
 });
