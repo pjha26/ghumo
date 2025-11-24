093f86f90966cfa7121243d429fe4bc065552348
pjha26 <praj80624@gmail.com>
2025-11-25 03:46:38 +0530

    added stripe for payment gateway

--- src/app/api/create-checkout-session/route.js
+++ src/app/api/create-checkout-session/route.js
@@ -2,7 +2,9 @@ import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
 import { currentUser } from '@clerk/nextjs/server';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2023-10-16',
+});
 
 export async function POST(req) {
     try {
